bfa5075aacdf318a20d04ad1454c9dd0fe2b3baf
erkgd <alejandrogarrancho@gmail.com>
2025-04-27 19:41:42 +0200

    feat: Add MusicMetadata widget to display artist and song name with styling

---
+++ myqx_app/lib/presentation/widgets/spotify/music_metadata.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MusicMetadata extends StatelessWidget {
+  final String artist;
+  final String musicname;
+
+  const MusicMetadata({
+    Key? key,
+    required this.artist,
+    required this.musicname,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 6.0), // Agregar padding izquierdo para los metadatos
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //-------------------------Artista
+          Text(
+            artist,
+            textAlign: TextAlign.left,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 15.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 2), // Pequeño espacio entre artista y nombre de canción
+          //-------------------------MUSICA
+          Text(
+            musicname,
+            textAlign: TextAlign.left,
+            style: const TextStyle(
+              color: Colors.white70,
+              fontSize: 12.0,
+            ),
+            overflow: TextOverflow.ellipsis, // Trunca el texto si es demasiado largo
+          ),
+        ],
+      ),
+    );
+  }
+}
